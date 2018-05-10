a4b634238a047c3d86fbcfec63f6069f9ab37b83
Jacob Janak <jakeathotmail@gmail.com>
2018-05-10 14:13:46 -0700

    Added React routing and imroved file structure to support multiple pages. Also, added build/ to the gitignore.

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Temp from './pages/Temp';
 
-class App extends Component {
+
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React, Jacob Janak</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload. Or, don't.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={Temp} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
-}
+};
 
 export default App;
---
+++ client/src/pages/Temp/Temp.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+
+const DeleteBtn = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React!</h1>
+    </header>
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload. Or, don't.
+    </p>
+  </div>
+);
+
+export default DeleteBtn;
